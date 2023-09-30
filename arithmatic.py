5484b0b4b0382ea147affa0d451709250d1babe0
PRASHANT KUMAR <68775732+prashantk137@users.noreply.github.com>
2023-09-30 11:46:50 +0530

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,8 @@
+def test1(a,b):
+    return a+b
+def test2(b,c):
+    return b*c
+def test3(d,e):
+    return d/e
+def test4():
+    print("This is my first module")
---
+++ test22.py
@@ -0,0 +1,9 @@
+def test(a,b):
+    return a+b
+def test1(b,c):
+    return b*c
+def test2(d,e):
+    return d/e
+def test3():
+    print("This is my first module")
+    
